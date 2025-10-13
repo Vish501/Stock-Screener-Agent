123e52eeabee2a97479f6688749775cf8df2b5d1
Vish501 <ladhavishal99@gmail.com>
2025-10-13 19:40:55 +0530

    Project structure

---
+++ app.py
@@ -0,0 +1,14 @@
+
+
+
+# 1. Create LLM
+# 2. Create State
+# 3. Build LLM Nodes
+# 4. Assembling Graph Structure
+# 5. Adding Memory and Compiling Graph
+# 6. Call Loop and Running the Loop
+# 7. Create Tool
+# 8. Bind LLM with Tools
+# 9. Create Tool Node
+# 10. Create Router Node
+# 11. Update Graph for Tools
