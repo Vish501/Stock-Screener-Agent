2589322449a92fd43939f5e713659ab48e0bdd43
Vish501 <ladhavishal99@gmail.com>
2025-10-14 19:57:39 +0530

    Updated

--- app.py
+++ app.py
@@ -5,9 +5,17 @@ from langgraph.checkpoint.memory import InMemorySaver
 from langchain_ollama import ChatOllama
 from colorama import Fore
 
-# 1. Create LLM
+# 1. Initialize LLM
+llm = ChatOllama(model="qwen2.5:14b") # Q4_K_M
+
 # 2. Create State
+class State(dict):
+    messages: Annotated[list, add_messages]
+
 # 3. Build LLM Nodes
+def chatbot(state: State):
+    return {"messages":[llm.invoke(state["messages"])]}
+
 # 4. Assembling Graph Structure
 # 5. Adding Memory and Compiling Graph
 # 6. Call Loop and Running the Loop
